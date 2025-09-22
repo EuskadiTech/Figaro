9ce68ed2afbfc14673374ffae13078355692df00
Naiel <109038805+naielv@users.noreply.github.com>
2025-09-22 13:06:45 +0200

    Add link to manage files in admin panel

--- www/admin/index.php
+++ www/admin/index.php
@@ -8,3 +8,4 @@ require_once "../_incl/pre-body.php";
 <a href="/materiales/allcentros_report.php" class="button">Informe de Materiales por Centro</a>
 <a href="/admin/centros.php" class="button">Gestionar Centros</a>
 <a href="/admin/usuarios.php" class="button">Gestionar Usuarios</a>
+<a href="/admin/files.php" class="button">Archivos</a>
