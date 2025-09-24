5a9b7fb763260ddbe6a843248983ca0ca2516ab6
Naiel <109038805+naielv@users.noreply.github.com>
2025-09-24 15:47:25 +0200

    Refactor file path to use RUTA_DATOS variable

--- www/actividades/eliminar_actividad.php
+++ www/actividades/eliminar_actividad.php
@@ -5,9 +5,9 @@ if (isset($_GET['id'])) {
     $id = $_GET['id'];
     $current_centro = $_SESSION['centro'];
     $aula = $_SESSION['aula'];
-    $file_path = "../../data/Actividades/$current_centro/$aula/$id.json";
+    $file_path = "$RUTA_DATOS/Actividades/$current_centro/$aula/$id.json";
     if ($_GET['global'] == '1') {
-        $file_path = "../../data/Actividades/_Global/$id.json";
+        $file_path = "$RUTA_DATOS/Actividades/_Global/$id.json";
     }
     if (file_exists($file_path)) {
         unlink($file_path);
